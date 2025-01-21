042102d4555f3849bfcaffb0f217999a105a8027
shyun <tjdgh0312@naver.com>
2025-01-21 12:56:11 +0900

    first commit

---
+++ src/main/java/com/loan/loan/dto/ResultObject.java
@@ -0,0 +1,38 @@
+package com.loan.loan.dto;
+
+import com.loan.loan.exception.BaseException;
+import com.loan.loan.exception.ResultType;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResultObject implements Serializable {
+
+    public String code;
+
+    public String desc;
+
+    public ResultObject(ResultType resultType) {
+        this.code = resultType.getCode();
+        this.desc = resultType.getDesc();
+    }
+
+    public ResultObject(ResultType resultCode, String message) {
+        this.code = resultCode.getCode();
+        this.desc = message;
+    }
+
+    public ResultObject(BaseException e) {
+        this.code = e.getCode();
+        this.desc = e.getDesc();
+    }
+
+    public static ResultObject getSuccess() {
+        return new ResultObject(ResultType.SUCCESS);
+    }
+}
